2feec1b1c555eacbacb7a4c674ce3cce7c7c96f7
Nur Mohammad <mohammadnur5480@gmail.com>
2023-11-06 21:49:15 +0600

    feat: added drag image select function

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,15 +1,25 @@
 import { useState } from "react";
 import { galleryImages } from "../../data/images";
 import { IImage } from "../../interfaces/interface";
+import ShowImage from "../ShowImage";
 
 const Gallery = () => {
   // get all data
     const [images, setImages] = useState(galleryImages as IImage[]);
+ const [selectedImages, setSelectedImages] = useState([] as string[]);
 
     // store selected item
     const [dragItem, setDragItem] = useState<IImage | null>(null);
     const [dragId, setDragId] = useState<string | null>(null);
-    console.log(dragId)
+
+    const handleItemClick = (imageId:string) => {
+      if (selectedImages.includes(imageId)) {
+        const currentImage=selectedImages.filter((id) => id !== imageId)
+        setSelectedImages(currentImage);
+      } else {
+        setSelectedImages([...selectedImages, imageId]);
+      }
+    };
 
     const handleItemDragStart = (e: React.DragEvent, image: IImage) => {
       e.dataTransfer.setData("itemId", image.id);
@@ -63,6 +73,36 @@ const Gallery = () => {
                 className="w-full bg-white h-full hover:opacity-[0.6]  rounded duration-200"
                 alt={`Image ${image.id}`}
               />
+               <div className="absolute w-full  h-full bg-black top-0 left-0 opacity-0  hover:opacity-50 duration-300 cursor-move">
+                <input
+                  checked={false}
+                  readOnly
+                  onClick={() => handleItemClick(image.id)}
+                  className="h-5 w-5 ml-5 mt-5 cursor-pointer"
+                  type="checkbox"
+                  name="checkbox"
+                  id={image.id}
+                />
+              </div>
+              {/* drag show image  */}
+              {dragId === image.id && (
+                <ShowImage image={dragItem} />
+              )}
+              {/* selected items */}
+              {selectedImages.includes(image.id) && (
+              <div
+                className={` absolute w-full h-full bg-black top-0 left-0 opacity-30 cursor-auto`}
+              >
+                <input
+                  id={image.id}
+                  defaultChecked
+                  onClick={() => handleItemClick(image.id)}
+                  className="h-5 w-5 ml-5 mt-5 cursor-pointer"
+                  type="checkbox"
+                  name="checkbox2"
+                />
+              </div>
+            )}
             </div>
           </div>
         ))}
--- src/components/ShowImage/ShowImage.tsx
+++ src/components/ShowImage/ShowImage.tsx
@@ -1,7 +1,9 @@
-const ShowImage = ({image}:{image:string}) => {
+import { IImage } from "../../interfaces/interface"
+
+const ShowImage = ({image}:{ image: IImage | null; }) => {
   return (
     <div className='absolute z-50 left-0 w-full  h-full bg-black top-0 duration-200'>
-    <img className='w-full h-full' src={image} alt="" /> 
+    <img className='w-full h-full' src={image?.img} alt="" /> 
 </div>
   )
 }
