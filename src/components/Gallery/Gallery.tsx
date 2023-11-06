3018c2201810adbae51d38219ddaf31aee0b90ee
Nur Mohammad <mohammadnur5480@gmail.com>
2023-11-06 20:26:40 +0600

    feat: added gallery item

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,6 +1,32 @@
+import { useState } from "react";
+import { galleryImages } from "../../data/images";
+import { IImage } from "../../interfaces/interface";
+
 const Gallery = () => {
+    const [images, setImages] = useState(galleryImages as IImage[]);
   return (
-    <div>Gallery</div>
+    <div className="root-gallery  pb-5 px-2">
+       <div className="items">
+        {images?.map((image) => (
+          <div
+            className="item"
+            key={image.id}
+          >
+            <div
+              className="w-full h-full"
+              draggable="true"
+            >
+              <img
+                src={image.img}
+                className={` hover:opacity-[0.5] w-full h-full rounded duration-300 bg-white`}
+                alt={`Image ${image.id}`}
+              />
+
+            </div>
+          </div>
+        ))}
+        </div>
+    </div>
   )
 }
 export default Gallery
